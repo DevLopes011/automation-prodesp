f466b738853584052065e71b6c9687458f5b664f
João Lopes <joao.arl.99@gmail.com>
2023-07-25 15:17:28 -0300

    feat: Add gitignore

---
+++ Infra/bucket_s3.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "prodesp_bucket" {
+  bucket = "my-tf-prodesp-bucket"
+}
+
+resource "aws_s3_bucket" "prodesp_azul" {
+  bucket = "Azul-Junta-Comercial"
+}
+
+resource "aws_s3_bucket" "prodesp_riachuelo" {
+  bucket = "Riachuelo-Junta-Comercial"
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "prodesp_bucket" {
+  bucket = aws_s3_bucket.prodesp_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "prodesp_bucket" {
+  bucket = aws_s3_bucket.prodesp_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "prodesp_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.prodesp_bucket,
+    aws_s3_bucket_public_access_block.prodesp_bucket,
+  ]
+
+  bucket = aws_s3_bucket.prodesp_bucket.id
+  acl    = "private"
+}
--- Web Automation/app/utils/PdfDownload.py
+++ Web Automation/app/utils/PdfDownload.py
@@ -1,18 +1,21 @@
 import requests
 
+
 class PdfDownloader:
     def __init__(self):
         pass
     
     def download_pdf(self, urls):
+        pdf_contents = [] 
+
         for url in urls:
             response = requests.get(url)
 
             if response.status_code == 200:
                 pdf_content = response.content
-
-                print(f"PDF '{pdf_content}' salvo com sucesso!")
+                pdf_contents.append(pdf_content)  # Adiciona o conteúdo do PDF à lista
+                print(f"PDF '{url}' baixado com sucesso!")
             else:
                 print(f"Erro ao obter o PDF da URL '{url}':", response.status_code)
         
-        return pdf_content
+        return pdf_contents  # Retorna a lista de conteúdos dos PDFs
